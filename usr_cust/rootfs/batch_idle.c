b4ebe3205fa238417ee8150a6514c86c0e48b3f1
Xingxing Wang <stellarwxx@163.com>
2019-09-29 01:09:50 +0800

    usr_cust: rootfs: add log to indicate batch_idle process is running

--- usr_cust/rootfs/batch_idle.c
+++ usr_cust/rootfs/batch_idle.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void)
+static int print_verb = 0;
+
+int main(int argc, char *argv[])
 {
+	if (argc > 1) {
+		if (!strncmp(argv[1], "-p", 2)) {
+			print_verb = 1;
+		}
+	}
 	int i = 0;
 	while(1) {
+		if (print_verb && i == 0) {
+			printf("batch_idle process is running\n");
+		}
 		i ++;
 	}
 	return 0;
