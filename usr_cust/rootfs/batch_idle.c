820499765a4b8f954179d2efcf70e399d8ebaac1
Xingxing Wang <stellarwxx@163.com>
2019-10-13 19:25:44 +0800

    usr_cust: batch_idle: add to print process pid

--- usr_cust/rootfs/batch_idle.c
+++ usr_cust/rootfs/batch_idle.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 static int print_verb = 0;
 
 int main(int argc, char *argv[])
 {
+	int pid = getpid();
 	if (argc > 1) {
 		if (!strncmp(argv[1], "-p", 2)) {
 			print_verb = 1;
@@ -14,7 +17,7 @@ int main(int argc, char *argv[])
 	int i = 0;
 	while(1) {
 		if (print_verb && i == 0) {
-			printf("batch_idle process is running\n");
+			printf("batch_idle process is running, pid : %d\n", pid);
 		}
 		i ++;
 	}
