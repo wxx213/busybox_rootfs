bb57bcbba0018e591958d4c651f7453716786a81
Xingxing Wang <stellarwxx@163.com>
2020-04-06 19:02:32 +0800

    usr_cust: rootfs: make batch_idle can fork new process by sending signal

--- usr_cust/rootfs/batch_idle.c
+++ usr_cust/rootfs/batch_idle.c
@@ -3,23 +3,46 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <signal.h>
 
 static int print_verb = 0;
+static int fork_new = 0;
+
+void (siguser1handler)(int sig)
+{
+	if ( sig == SIGUSR1) {
+		fork_new = 1;
+	}
+}
 
 int main(int argc, char *argv[])
 {
-	int pid = getpid();
+	pid_t pid = getpid();
 	if (argc > 1) {
 		if (!strncmp(argv[1], "-p", 2)) {
 			print_verb = 1;
 		}
 	}
+	if (signal(SIGUSR1, siguser1handler) == SIG_ERR) {
+		printf("signal SIGUSR error\n");
+	}
 	int i = 0;
 	while(1) {
-		if (print_verb && i == 0) {
-			printf("batch_idle process is running, pid : %d\n", pid);
+		if (0 == i) {
+			if (print_verb) {
+				printf("batch_idle process is running, pid : %d\n", pid);
+			}
+			if (fork_new) {
+				fork_new = 0;
+				pid = fork();
+				if (pid < 0) {
+					printf("fork new process error\n");
+				} else if (pid == 0) {
+					while(1) i++;
+				}
+			}
 		}
-		i ++;
+		i++;
 	}
 	return 0;
 }
